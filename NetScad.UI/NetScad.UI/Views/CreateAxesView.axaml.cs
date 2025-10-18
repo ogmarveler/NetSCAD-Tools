627ca8daa190cce46da4aeae07c1807f2da9b249
ogmarveler <no-reply@marveler.com>
2025-10-18 02:02:06 +0000

    Updated styling

--- NetScad.UI/NetScad.UI/Views/CreateAxesView.axaml.cs
+++ NetScad.UI/NetScad.UI/Views/CreateAxesView.axaml.cs
@@ -20,12 +20,46 @@ public partial class CreateAxesView : UserControl, INotifyPropertyChanged
     {
         await ViewModel.ConvertInputs(ViewModel._decimalPlaces);
     }
+
     private async void _CreateCustomAxisAsync(object? sender, RoutedEventArgs e)
     {
         await ViewModel.CreateCustomAxisAsync();
     }
+
     private async void _ClearInputsAsync(object? sender, RoutedEventArgs e)
     {
         await ViewModel.ClearInputs();
+    }
+
+    /// <summary>
+    /// Handles auto-generating columns for the AxesList DataGrid
+    /// Customizes column headers to be more user-friendly
+    /// </summary>
+    private void DataGrid_AutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
+    {
+        // Customize column headers based on property name
+        e.Column.Header = e.PropertyName switch
+        {
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.CallingMethod) => "Module Call",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.Unit) => "Unit Type",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.Theme) => "Theme",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.RangeX) => "Range X",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.RangeY) => "Range Y",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.RangeZ) => "Range Z",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MinX) => "Min X",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MaxX) => "Max X",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MinY) => "Min Y",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MaxY) => "Max Y",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MinZ) => "Min Z",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.MaxZ) => "Max Z",
+            nameof(NetScad.Axis.Scad.Models.GeneratedModule.Volume) => "Volume",
+            _ => e.PropertyName // Default to property name if not matched
+        };
+
+        // Optional: Set column width based on content type
+        if (e.PropertyName == nameof(NetScad.Axis.Scad.Models.GeneratedModule.CallingMethod))
+        {
+            e.Column.Width = new DataGridLength(1, DataGridLengthUnitType.Star); // Take remaining space
+        }
     }
 }
