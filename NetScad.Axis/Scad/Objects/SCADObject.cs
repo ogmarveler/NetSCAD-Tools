da00aec8fb274ba615ffa564ff7f353261f0db7a
ogmarveler <no-reply@marveler.com>
2025-10-20 09:58:20 +0000

    Code cleanup

--- NetScad.Axis/Scad/Objects/SCADObject.cs
+++ NetScad.Axis/Scad/Objects/SCADObject.cs
@@ -6,7 +6,7 @@ namespace NetScad.Axis.SCAD.Objects
     {
         public static string Module(string name, string content = "", List<string>? parameters = null)
         {
-            return $"{BlockStatement.GetModule(name: name, parameters: parameters, content: content)}\n";
+            return $"{BlockStatement.GetModule(name: name, parameters: parameters ?? [], content: content)}\n";
         }
     }
 }
